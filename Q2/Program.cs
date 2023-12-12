97405e595fa53d2b4cc83aae1af9780a00fa4213
raphaelcanadazy <raphaelcanadazy@gmail.com>
2023-12-11 20:54:27 -0500

    FinalTestQ2

---
+++ Q2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace Q2
+{
+
+    public class Program
+    {
+        public delegate void EventHandler(string name, int value);
+        // 1.create a class to pass as an argument for the event handlers (EventArgs class)
+        public class Q2EventArgs: EventArgs
+        {
+            public string Name { get; set; }
+            public int Value { get; set; }
+            public Q2EventArgs(string name, int value) 
+            {
+                this.Name = name;
+                this.Value = value;
+            }
+        }
+
+        public class  Q2Publisher
+        {
+            public event EventHandler<Q2EventArgs> Q2Pub;
+
+            public void Publish(Q2EventArgs e)
+            {
+                Raise(e);
+            }
+
+            protected virtual void Raise(Q2EventArgs e)
+            {
+                Q2Pub?.Invoke(this, e);
+            }
+        }
+
+        public class Q2Subscriber
+        {
+            public void Q2Action(object sender, Q2EventArgs e)
+            {
+                Console.WriteLine("Q2Subscriber successed");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
